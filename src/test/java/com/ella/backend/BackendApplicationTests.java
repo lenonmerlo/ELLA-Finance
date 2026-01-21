7540c8ac947de51edb4aed8885f57453e3e510d8
lenonmerlo <lenontm@gmail.com>
2026-01-21 08:17:16 -0300

    test(app): adjust backend application test

--- src/test/java/com/ella/backend/BackendApplicationTests.java
+++ src/test/java/com/ella/backend/BackendApplicationTests.java
@@ -3,7 +3,15 @@ package com.ella.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.flyway.enabled=false",
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.datasource.url=jdbc:h2:mem:ella_test;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class BackendApplicationTests {
 
 	@Test
